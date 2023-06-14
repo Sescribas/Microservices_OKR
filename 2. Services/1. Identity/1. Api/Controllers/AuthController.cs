cb44e180d390eca6fde12f9267affc9ddec08df1
Sescribas <santi.escribas@hotmail.com>
2023-06-13 22:08:16 -0300

    Add the login endpoint

--- 2. Services/1. Identity/1. Api/Controllers/AuthController.cs
+++ 2. Services/1. Identity/1. Api/Controllers/AuthController.cs
@@ -21,19 +21,55 @@ namespace Identity.api.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserModel>> Register(UserDto request) 
         {
+            _logger.LogInformation("Comienza la operacion de registracion.");
+
             CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passworSalt);
             User.UserName = request.UserName;
             User.PasswordHash = passwordHash;
             User.PasswordSalt = passworSalt;
+
+            _logger.LogInformation("Finaliza la operacion de registracion.");
+
             return Ok(User);
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if(User.UserName != request.UserName)
+            {
+                return BadRequest("No existe el usuario con ese nombre de usuario.");
+            }
+
+            if (!VerifyPassword(request.Password, User.PasswordHash, User.PasswordSalt))
+            {
+                return BadRequest("El usuario o la contraseña es incorrecta.");
+
+            }
+
+            return Ok("Login");
+        }
+
+
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passworSalt)
         {
+            _logger.LogInformation("Comienza la operacion de hasheo.");
+
             using (var hmac = new HMACSHA512())
             {
                 passworSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            _logger.LogInformation("Finaliza la operacion de hasheo.");
+
+        }
+
+        private bool VerifyPassword(string password, byte[] passwordHash, byte[] passworSalt)
+        {
+            using (var hmac = new HMACSHA512(User.PasswordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
     }
